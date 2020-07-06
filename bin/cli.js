da4915f2d5dce18e5a5a67b28f2d7402f322785c
Adem Gaygusuz <adem@ardweb.co.uk>
2020-07-06 22:05:28 +0100

    Remove nodemon from server start up

--- bin/cli.js
+++ bin/cli.js
@@ -5,7 +5,7 @@ const concurrently = require('concurrently');
 const testbookRootPath = `${__dirname}/..`;
 
 concurrently([
-  `${testbookRootPath}/node_modules/.bin/nodemon --exec ${testbookRootPath}/node_modules/.bin/babel-node ${testbookRootPath}/src/server.js`,
+  `${testbookRootPath}/node_modules/.bin/babel-node ${testbookRootPath}/src/server.js`,
   `npm start --prefix ${__dirname}/../`,
 ], {
   prefix: 'name',
