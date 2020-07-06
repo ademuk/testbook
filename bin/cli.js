710e0381cfaf59e8d21c6d1aa5c717dcde648bf9
Adem Gaygusuz <adem@ardweb.co.uk>
2020-07-07 00:34:22 +0100

    Add default NODE_ENV

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,8 @@ const concurrently = require('concurrently');
 
 const testbookRootPath = `${__dirname}/..`;
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 concurrently([
   `${testbookRootPath}/node_modules/.bin/babel-node ${testbookRootPath}/src/server.js`,
   `npm start --prefix ${__dirname}/../`,
