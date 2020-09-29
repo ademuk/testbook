8adf79b0000f76ad877895350b939852120fdb79
Adem Gaygusuz <adem@ardweb.co.uk>
2020-09-29 17:11:54 +0100

    Remove commented jsx

--- src/Components.js
+++ src/Components.js
@@ -10,19 +10,6 @@ const Module = ({module}) => (
             {module.file}
           </div>
 
-          {/*<div className="flex items-center bg-gray-200 rounded-md">*/}
-          {/*  <div className="pl-2">*/}
-          {/*    <svg className="fill-current text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg"*/}
-          {/*         viewBox="0 0 24 24">*/}
-          {/*      <path className="heroicon-ui"*/}
-          {/*            d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/>*/}
-          {/*    </svg>*/}
-          {/*  </div>*/}
-          {/*  <input*/}
-          {/*    className="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"*/}
-          {/*    id="search" type="text" placeholder="Search teams or members" />*/}
-          {/*</div>*/}
-
           <div className="py-3">
           {module.components.map(c =>
             <StatusLink
@@ -34,14 +21,6 @@ const Module = ({module}) => (
           )}
           </div>
 
-          {/*<div className="block bg-gray-100 text-sm text-right py-2 px-3 -mx-3 -mb-2 rounded-b-lg">*/}
-          {/*  <button className="hover:text-gray-600 text-gray-500 font-bold py-2 px-4">*/}
-          {/*    Cancel*/}
-          {/*  </button>*/}
-          {/*  <button className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded">*/}
-          {/*    Invite*/}
-          {/*  </button>*/}
-          {/*</div>*/}
         </div>
       </div>
     </div>
