7cf61e96515ea0811e5e04e155a0a5369510e1a5
Adem Gaygusuz <adem@ardweb.co.uk>
2020-11-11 12:51:51 +0000

    feat: Add Typescript support
    Support testing of React components written in TypeScript, along with Prop type detection.

--- server/devServer.ts
+++ server/devServer.ts
@@ -3,21 +3,21 @@ const {spawn} = require("child_process");
 
 const startDevServer = (client_static_root) => {
   const node_modules_path = path.dirname(path.dirname(require.resolve('ts-node/package.json')));
-  const ls = spawn(`${node_modules_path}/.bin/ts-node-dev`, [`${require.resolve('../server.ts')}`, client_static_root]);
+  const process = spawn(`${node_modules_path}/.bin/ts-node-dev`, ['--pretty', '--project', require.resolve('../tsconfig.json'), require.resolve('../server.ts'), client_static_root]);
 
-  ls.stdout.on("data", data => {
+  process.stdout.on("data", data => {
     console.log(`stdout: ${data}`);
   });
 
-  ls.stderr.on("data", data => {
+  process.stderr.on("data", data => {
     console.log(`stderr: ${data}`);
   });
 
-  ls.on('error', (error) => {
+  process.on('error', (error) => {
     console.log(`error: ${error.message}`);
   });
 
-  ls.on("close", code => {
+  process.on("close", code => {
     console.log(`child process exited with code ${code}`);
   });
 
